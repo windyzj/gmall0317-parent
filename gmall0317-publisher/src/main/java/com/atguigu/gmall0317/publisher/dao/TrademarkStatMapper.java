dea0abe65bd97b8844b605ce2d804f838dd71013
zhangchen <windyzj@163.com>
2020-09-10 19:50:23 +0800

    数据接口，补传

---
+++ gmall0317-publisher/src/main/java/com/atguigu/gmall0317/publisher/dao/TrademarkStatMapper.java
@@ -0,0 +1,9 @@
+package com.atguigu.gmall0317.publisher.dao;
+
+import java.util.List;
+import java.util.Map;
+
+public interface TrademarkStatMapper {
+
+    public List<Map> selectTrademarkStat(String date);
+}
